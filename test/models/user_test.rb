4a61cfe9e172d2ed3c4acd74cd9e4e5dda0bc231
Fritz Meyer <meyerfr@gmx.de>
2020-05-01 10:18:13 +0200

    added test for user model #is_role(role) method

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,11 @@ class UserTest < ActiveSupport::TestCase
     user = users(:one)
     assert_equal 'John Lennon', user.full_name
   end
+
+  test 'is_role(role) returns true if role == user.role' do
+    user = users(:one)
+    assert_equal true, user.is_role?('sitter')
+    assert_equal false, user.is_role?('parent')
+    assert_equal false, user.is_role?('admin')
+  end
 end
