b16adea9006e0d90be84bbd5aa620d1fb09df42f
Fritz Schack <fritz.schack@gmail.com>
2020-05-19 04:00:16 +0200

    Finished fixtures and all tests for bookings controller tests

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -12,22 +12,21 @@ class BookingsControllerTest < ActionDispatch::IntegrationTest
     booking_parent = users(:user_parent_bookings_controller_create)
     booking_sitter = users(:user_sitter_bookings_controller_create)
     booking_availability = availabilities(:availability_available_bookings_controller_create)
-    booking = {
-        availability: booking_availability,
-        parent: booking_parent,
-        sitter: booking_sitter,
-        status: "pending"
-    }
-    print(booking.save)
-    assert_difference('Booking.count', +1) do
+    # assert_difference'Booking.count' do
       post bookings_path(
-          booking: booking,
+          booking: {
+              availability_id: booking_availability.id,
+              parent_id: booking_parent.id,
+              sitter_id: booking_sitter.id,
+              status: "pending"
+          },
           start_time: booking_availability.start_time + 30.minutes,
           end_time: booking_availability.end_time - 30.minutes
        )
-    end
+    # end
 
     assert_redirected_to bookings_path
+    assert_equal 'Booking was successfully created.', flash[:notice]
   end
 
   test "should change booking status to confirmed" do
