d98b6e556167bdc4fa6322de7bd45a05eb0f500b
Fritz Schack <fritz.schack@gmail.com>
2020-05-19 03:49:30 +0200

    Some small changes to the users and bookings controller tests

--- test/controllers/bookings_controller_test.rb
+++ test/controllers/bookings_controller_test.rb
@@ -9,27 +9,30 @@ class BookingsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create booking" do
     sign_in(users(:user_parent_bookings_controller_create))
+    booking_parent = users(:user_parent_bookings_controller_create)
     booking_sitter = users(:user_sitter_bookings_controller_create)
     booking_availability = availabilities(:availability_available_bookings_controller_create)
     assert_difference('Booking.count', +1) do
-      post bookings_url, params: {
+      post bookings_path(
           booking: {
               availability: booking_availability,
+              parent: booking_parent,
               sitter: booking_sitter,
               status: "pending"
           },
-          start_time: booking_availability.start_time,
-          end_time: booking_availability.end_time
-      }
+          start_time: booking_availability.start_time + 30.minutes,
+          end_time: booking_availability.end_time - 30.minutes
+       )
     end
 
-    assert_redirected_to bookings_url
+    assert_redirected_to bookings_path
   end
 
   test "should change booking status to confirmed" do
     sign_in(users(:user_parent_bookings_controller_confirm))
     booking = bookings(:booking_pending_bookings_controller_confirm)
     patch confirm_booking_path(booking)
+    booking.reload
     assert_equal("confirmed", booking.status)
   end
 
@@ -37,6 +40,7 @@ class BookingsControllerTest < ActionDispatch::IntegrationTest
     sign_in(users(:user_parent_bookings_controller_decline))
     booking = bookings(:booking_pending_bookings_controller_decline)
     patch decline_booking_path(booking)
+    booking.reload
     assert_equal("declined", booking.status)
   end
 end
