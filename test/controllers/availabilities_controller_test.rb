12582311aa793821a472ca3d4cb20a60aae8ca28
Fritz Schack <fritz.schack@gmail.com>
2020-05-19 02:48:21 +0200

    Finished fixtures for availabilities controller tests

--- test/controllers/availabilities_controller_test.rb
+++ test/controllers/availabilities_controller_test.rb
@@ -1,133 +1,124 @@
 require 'test_helper'
 
 class AvailabilitiesControllerTest < ActionDispatch::IntegrationTest
-  # Setup
-  setup do
-    @availability_one = availabilities(:availability_one)
-    @availability_two = availabilities(:availability_two)
-    @availability_three = availabilities(:availability_three)
-
-    @user_parent = users(:meyer)
-    @user_sitter = users(:schack)
-    @user_sitter_2 = users(:lennon)
-    @user_sitter_3 = users(:marley)
-
-    # sign in user
-    get new_user_session_url
-    sign_in(@user_sitter)
-    post user_session_url
-
-    follow_redirect!
-    assert_response :success
-  end
-
   # Index Tests
   test "should get index" do
-    get availabilities_url
+    sign_in(users(:user_sitter_availabilities_controller_index))
+    get availabilities_path
     assert_response :success
     assert_template 'availabilities/index'
   end
 
   # Create Tests
   test 'should create availability' do
+    sign_in(users(:user_sitter_availabilities_controller_create))
     assert_difference('Availability.count', +1) do
-      post availabilities_url, params: {
+      post availabilities_path, params: {
           date: "2020-06-04",
           start_time: "10:00:00",
           end_time: "18:00:00",
       }
     end
 
-    assert_redirected_to availabilities_url
+    assert_redirected_to availabilities_path
   end
 
   test 'should not create availability with end_time less than three hours later than start_time' do
+    sign_in(users(:user_sitter_availabilities_controller_create))
     assert_difference('Availability.count', 0) do
-      post availabilities_url, params: {
+      post availabilities_path, params: {
           date: "2020-06-04",
           start_time: "10:00:00",
           end_time: "12:00:00",
       }
     end
 
-    assert_redirected_to availabilities_url
+    assert_redirected_to availabilities_path
   end
 
   test 'should not create availability with start_time or end_time in the past' do
+    sign_in(users(:user_sitter_availabilities_controller_create))
     assert_difference('Availability.count', 0) do
-      post availabilities_url, params: {
+      post availabilities_path, params: {
           date: Date.today.change(day: Date.today.day - 1),
           start_time: "10:00:00",
           end_time: "12:00:00",
       }
     end
 
-    assert_redirected_to availabilities_url
+    assert_redirected_to availabilities_path
   end
 
   test 'should not create availabilities with overlapping times' do
+    sign_in(users(:user_sitter_availabilities_controller_create))
     assert_difference('Availability.count', +1) do
-      post availabilities_url, params: {
+      post availabilities_path, params: {
           date: "2020-06-04",
           start_time: "17:00:00",
           end_time: "20:00:00",
       }
     end
 
-    assert_redirected_to availabilities_url
+    assert_redirected_to availabilities_path
   end
 
   # Edit Tests
   test "should get edit" do
-    get edit_availability_url(@availability_three)
+    sign_in(users(:user_sitter_availabilities_controller_edit_update_destroy))
+    get edit_availability_url(availabilities(:availability_valid_availabilities_controller_edit_update_destroy))
     assert_response :success
   end
 
   # Update Tests
   test 'should update availability' do
-    patch availability_url(@availability_three), params: {
+    sign_in(users(:user_sitter_availabilities_controller_edit_update_destroy))
+    patch availability_url(availabilities(:availability_valid_availabilities_controller_edit_update_destroy)), params: {
         date: "2020-06-03",
         start_time: "10:00:00",
         end_time: "18:00:00",
     }
 
-    assert_redirected_to availabilities_url
+    assert_redirected_to availabilities_path
   end
 
   test 'should not update availability with end_time less than three hours later than start_time' do
-    patch availability_url(@availability_three), params: {
+    sign_in(users(:user_sitter_availabilities_controller_edit_update_destroy))
+    patch availability_url(availabilities(:availability_valid_availabilities_controller_edit_update_destroy)), params: {
         date: "2020-06-04",
         start_time: "10:00:00",
         end_time: "12:00:00",
     }
 
-    assert_redirected_to availabilities_url
+    assert_redirected_to availabilities_path
   end
 
   test 'should not update availability with start_time or end_time in the future' do
-    patch availability_url(@availability_three), params: {
+    sign_in(users(:user_sitter_availabilities_controller_edit_update_destroy))
+    patch availability_url(availabilities(:availability_valid_availabilities_controller_edit_update_destroy)), params: {
         date: Date.today.change(day: Date.today.day - 1),
         start_time: "10:00:00",
         end_time: "12:00:00",
     }
 
-    assert_redirected_to availabilities_url
+    assert_redirected_to availabilities_path
   end
 
   test 'should not update availabilities with overlapping times' do
-    patch availability_url(@availability_three), params: {
+    sign_in(users(:user_sitter_availabilities_controller_edit_update_destroy))
+    patch availability_url(availabilities(:availability_valid_availabilities_controller_edit_update_destroy)), params: {
         date: "2020-06-04",
         start_time: "17:00:00",
         end_time: "20:00:00",
     }
 
-    assert_redirected_to availabilities_url
+    assert_redirected_to availabilities_path
   end
 
   # Destroy Tests
   test 'should destroy availability' do
     assert_difference('Availability.count', -1) do
-      delete availability_url(@availability_three)
+      sign_in(users(:user_sitter_availabilities_controller_edit_update_destroy))
+      delete availability_url(availabilities(:availability_valid_availabilities_controller_edit_update_destroy))
     end
 
     assert_redirected_to availabilities_url
@@ -135,52 +126,35 @@ class AvailabilitiesControllerTest < ActionDispatch::IntegrationTest
 
   # Sitter Authentication Tests
   test 'should authenticate user with role "sitter"' do
-    get availabilities_url
+    sign_in(users(:user_sitter_availabilities_controller_authenticate))
+    get availabilities_path
     assert_response :success
   end
 
   test 'should not authenticate user with role "parent"' do
-    get new_user_session_url
-    sign_in(@user_parent)
-    post user_session_url
-
-    follow_redirect!
-    assert_response :success
-
-    get availabilities_url
-
-    assert_redirected_to root_url
+    sign_in(users(:user_parent_availabilities_controller_authenticate))
+    get availabilities_path
+    assert_redirected_to root_path
   end
 
   # Page Count Tests
-  test 'page counter should return 1 for > 10 availability' do
-    get availabilities_url
+  test 'page counter should return 1 for 15 availabilities' do
+    sign_in(users(:user_sitter_availabilities_controller_page_counter_15))
+    get availabilities_path
     assert_response :success
     assert_equal(1, assigns(:page_count))
   end
 
-  test 'page counter should return 0 for 1 availability' do
-    get new_user_session_url
-    sign_in(@user_sitter_2)
-    post user_session_url
-
-    follow_redirect!
-    assert_response :success
-
-    get availabilities_url
+  test 'page counter should return 0 for 3 availabilities' do
+    sign_in(users(:user_sitter_availabilities_controller_page_counter_3))
+    get availabilities_path
     assert_response :success
     assert_equal(0, assigns(:page_count))
   end
 
   test 'page counter should return 0 for 0 availabilities' do
-    get new_user_session_url
-    sign_in(@user_sitter_3)
-    post user_session_url
-
-    follow_redirect!
-    assert_response :success
-
-    get availabilities_url
+    sign_in(users(:user_sitter_availabilities_controller_page_counter_0))
+    get availabilities_path
     assert_response :success
     assert_equal(0, assigns(:page_count))
   end
