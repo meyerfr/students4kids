37f19605344c54c33218f629a9a32d3c4253e111
Fritz Meyer <meyerfr@gmx.de>
2020-05-21 11:30:27 +0200

    applied bookings is_status function everywhere and remove respond to formats in bookingsController

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,21 +15,18 @@ class BookingsController < ApplicationController
       past_bookings = current_user.sitter_bookings.select { |b| b.availability.start_time < DateTime.current }
     end
 
-    @bookings_pending = future_bookings.select { |booking| booking.status == 'pending' }
-    @bookings_confirmed = future_bookings.select { |booking| booking.status == 'confirmed' }
-    @bookings_past = past_bookings.select { |booking| booking.status == 'confirmed' }
+    @bookings_pending = future_bookings.select { |booking| booking.is_status?('pending') }
+    @bookings_confirmed = future_bookings.select { |booking| booking.is_status?('confirmed') }
+    @bookings_past = past_bookings.select { |booking| booking.is_status?('confirmed') }
   end
 
   def create
     @booking = current_user.parent_bookings.new(booking_params)
-
-    respond_to do |format|
-      if @booking.save
-        ActiveRecord::Base.connection.exec_query("CALL update_availability ('#{params[:start_time]}', '#{params[:end_time]}', #{@booking.availability.id})")
-        format.html { redirect_to bookings_path, notice: 'Booking was successfully created.' }
-      else
-        format.html { redirect_to bookings_path, notice: 'Booking could not be created. Please try again' }
-      end
+    if @booking.save
+      ActiveRecord::Base.connection.exec_query("CALL update_availability ('#{params[:start_time]}', '#{params[:end_time]}', #{@booking.availability.id})")
+      redirect_to bookings_path, notice: 'Booking was successfully created.'
+    else
+      redirect_to bookings_path, notice: 'Booking could not be created. Please try again'
     end
   end
 
