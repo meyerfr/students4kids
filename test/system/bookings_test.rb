be2f369cc8fbc6fbe091045c0537ec92e4d2c2be
Fritz Schack <fritz.schack@gmail.com>
2020-05-21 18:45:39 +0200

    Update of tests after CSS refactoring. Also upgraded the webdriver gem

--- test/system/bookings_test.rb
+++ test/system/bookings_test.rb
@@ -15,7 +15,7 @@ class BookingsTest < ApplicationSystemTestCase
     fill_in "end_time", with: "14:00:00"
     click_on "Search"
 
-    find(".sitter").hover
+    find(".listing").hover
     click_on "make-booking"
 
     click_on "submit-booking"
@@ -27,7 +27,7 @@ class BookingsTest < ApplicationSystemTestCase
     sign_in(users(:user_parent_bookings_e2e_sitter_profile))
     visit root_url
     click_on "Bookings"
-    find('.booking').hover
+    find('.listing').hover
     click_on "profile"
     assert_current_path(user_path(users(:user_sitter_bookings_e2e_sitter_profile)))
   end
@@ -37,9 +37,9 @@ class BookingsTest < ApplicationSystemTestCase
     visit root_url
     click_on "Bookings"
     assert_selector "h3", text: "Pending"
-    find('.booking').hover
+    find('.listing').hover
     click_on "decline"
-    assert_equal(0, page.all(:css, '.booking').count)
+    assert_equal(0, page.all(:css, '.availability').count)
   end
 
   # Testing Sitter User Stories
@@ -49,11 +49,11 @@ class BookingsTest < ApplicationSystemTestCase
     visit root_url
     click_on "Bookings"
     assert_selector "h3", text: "Pending"
-    find('.booking').hover
+    find('.listing').hover
     click_on "confirm"
     assert_selector "h3", text: "Confirmed"
     confirm_count = 0
-    find(".booking") do |booking|
+    find(".listing") do |booking|
       booking.hover
       if find("#decline") && find("#profile")
         confirm_count += 1
@@ -67,9 +67,9 @@ class BookingsTest < ApplicationSystemTestCase
     visit root_url
     click_on "Bookings"
     assert_selector "h3", text: "Pending"
-    find('.booking').hover
+    find('.listing').hover
     click_on "decline"
-    assert_equal(0, page.all(:css, '.booking').count)
+    assert_equal(0, page.all(:css, '.listing').count)
   end
 
   test "visit a booking's parent profile" do
@@ -77,7 +77,7 @@ class BookingsTest < ApplicationSystemTestCase
     visit root_url
     click_on "Bookings"
     assert_selector "h3", text: "Pending"
-    find('.booking').hover
+    find('.listing').hover
     click_on "profile"
     assert_current_path(user_path(users(:user_parent_bookings_e2e_parent_profile)))
   end
