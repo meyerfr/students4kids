4d54a1d48e2abe5241c39a6f3b160a04c514e14d
Fritz Schack <fritz.schack@gmail.com>
2020-05-22 17:00:57 +0200

    Refactored the booking model for readability and structure

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,10 @@
 class Booking < ApplicationRecord
+  # Associations
   belongs_to :parent, class_name: 'User'
   belongs_to :sitter, class_name: 'User'
   belongs_to :availability
 
+  # Model Functions
   def is_status?(status)
     self.status == status
   end
